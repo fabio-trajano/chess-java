94cc8299b3df4093f33f4659d91eca685c4c2ea7
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-09-03 16:03:17 +0100

    Fix - promotion

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -9,8 +9,6 @@ import pieces.Pawn;
 import pieces.Bishop;
 import pieces.Knight;
 import pieces.Queen;
-
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -122,7 +120,7 @@ public class ChessMatch {
             throw new IllegalStateException("There is no piece to be promoted");
         }
         if (!type.equals("B") && !type.equals("R") && !type.equals("N") && !type.equals("Q")) {
-            throw new InvalidParameterException("Invalid type for promotion");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
